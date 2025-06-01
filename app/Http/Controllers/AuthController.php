0809f188742ead274e4ff8ba8b3783a52bb8b12e
ValdinarLopes <valdinarlopespessoaneto@gmail.com>
2025-06-01 17:20:47 -0300

    WIP: controller de autenticacao feita e rotas atualizadas.

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illiminate\Validation\ValidationException;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:120',
+            'email' => 'required|string|email|max120|unique:users',
+            'password' => 'required|string|min:8'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['token' => $token], 201);
+    }
+
+    public function login(Requst $request)
+    {
+        $request->validade([
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)){
+            throw ValidationException::withMessages([
+                'email' => ['As credenciais estão incorretas.']
+            ]);
+        }
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['token' => $token]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAcessToken()->delete();
+        return response()->json(['message' => 'Logout realizado']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AutoController;
 use App\Http\Controller\PontoTuristicoController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+
 
 Route::get('/pontos-turisticos', [PontoTuristicoController::class, 'index']);
 Route::get('/pontos-turisticos/{id}', [PontoTuristicoController::class, 'show']);
